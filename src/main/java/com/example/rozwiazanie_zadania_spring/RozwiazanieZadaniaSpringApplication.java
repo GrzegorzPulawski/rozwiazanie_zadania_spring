6997e091eea23ea3ba8b9ee6ea6cc2b039c5c207
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2024-06-10 20:28:46 +0200

    pierwszy commit

---
+++ src/main/java/com/example/rozwiazanie_zadania_spring/RozwiazanieZadaniaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.rozwiazanie_zadania_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RozwiazanieZadaniaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RozwiazanieZadaniaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rozwiazanie_zadania_spring/RozwiazanieZadaniaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rozwiazanie_zadania_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RozwiazanieZadaniaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
